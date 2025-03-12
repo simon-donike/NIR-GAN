23b6625b6f63057547ce1212f5049ae45805aab0
Simon Donike <accounts@donike.net>
2025-03-12 16:33:22 +0000

    add plotting to val procedure, change to worldstrat

--- validation_utils/get_results_table.py
+++ validation_utils/get_results_table.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tqdm import tqdm
 from data.worldstrat import worldstrat_ds
 from data.s100k_dataset import S2_100k
+from data.worldstrat import worldstrat_ds
 from omegaconf import OmegaConf
 from torch.utils.data import DataLoader
 import os
@@ -27,8 +28,10 @@ def create_val_metrics(config_path,folder):
     config = OmegaConf.load(config_path)
     satclip = config.satclip.use_satclip
 
-    #ds = worldstrat_ds(config)
-    ds = S2_100k(config,phase="val")
+    ds = worldstrat_ds(config)
+    
+    #ds = S2_100k(config,phase="val")
+    ds = worldstrat_ds(config)
     dl = DataLoader(ds, batch_size=1, shuffle=True, num_workers=0)
 
     # Load Model
@@ -91,25 +94,32 @@ def create_val_metrics(config_path,folder):
         metrics_dict["l1_evi"].append(result_dict["indices_loss/evi_error"].item())
 
         # plot standard image
-        if id_%10==0:
+        if id_%50==0:
+            # Create Image folder and name
+            image_folder = os.path.join(folder,"images")
+            os.makedirs(image_folder,exist_ok=True)
+            type_name = "SatCLIP" if satclip else "NoSatCLIP"
+            im_path = os.path.join(image_folder,f"example_image_{id_}_{type_name}.png")
+            # get and save image
             img = plot_tensors(rgb, nir, pred,title="Worldstrat Validation")
-            img.save(f'validation_utils/images/example_image_{id_}.png', 'PNG')
+            img.save(im_path, 'PNG')
             
-            # get and plot Indices Image
-            val_obj.mode = "index"
-            # get indices
-            ndvi,ndvi_pred = val_obj.ndvi_calculation(rgb,nir,pred)
-            ndwi,ndwi_pred = val_obj.ndwi_calculation(rgb,nir,pred)
-            evi,evi_pred = val_obj.evi_calculation(rgb,nir,pred)
-            # create plots
-            im_ndvi = plot_index(rgb,ndvi,ndvi_pred,title="NDVI",index_name="NDVI")
-            im_ndwi = plot_index(rgb,ndwi,ndwi_pred,title="NDWI",index_name="NDWI")
-            im_evi = plot_index(rgb,evi,evi_pred,title="EVI",index_name="EVI")
-            # save plots
-            # TODO: Find out why all the indices look the same
-            im_ndvi.save(f'images/indices/{id_}_ndvi.png', 'PNG')
-            im_ndwi.save(f'images/indices/{id_}_ndwi.png', 'PNG')  
-            im_evi.save(f'images/indices/{id_}_evi.png', 'PNG')
+            if False:
+                # get and plot Indices Image
+                val_obj.mode = "index"
+                # get indices
+                ndvi,ndvi_pred = val_obj.ndvi_calculation(rgb,nir,pred)
+                ndwi,ndwi_pred = val_obj.ndwi_calculation(rgb,nir,pred)
+                evi,evi_pred = val_obj.evi_calculation(rgb,nir,pred)
+                # create plots
+                im_ndvi = plot_index(rgb,ndvi,ndvi_pred,title="NDVI",index_name="NDVI")
+                im_ndwi = plot_index(rgb,ndwi,ndwi_pred,title="NDWI",index_name="NDWI")
+                im_evi = plot_index(rgb,evi,evi_pred,title="EVI",index_name="EVI")
+                # save plots
+                # TODO: Find out why all the indices look the same
+                im_ndvi.save(f'images/indices/{id_}_ndvi.png', 'PNG')
+                im_ndwi.save(f'images/indices/{id_}_ndwi.png', 'PNG')  
+                im_evi.save(f'images/indices/{id_}_evi.png', 'PNG')
         
 
         # save metrics
@@ -137,7 +147,7 @@ def create_val_metrics(config_path,folder):
     
 
 def filter_for_countries(gdf):
-    world_countries = gpd.read_file("validation_utils/datasets/countries.geojson")
+    world_countries = gpd.read_file("validation_utils/datasets/countries_noAntGreen.geojson")
     gdf = gpd.sjoin(gdf,world_countries,how="inner")
     return gdf
 
